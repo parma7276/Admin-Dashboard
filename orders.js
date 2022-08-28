41f1c21fec3bbb4590bdfe6ede979af8813c6e3c
Parma <parmanandbhagat61@gmail.com>
2022-08-29 03:33:57 +0530

    Initial commit

---
+++ orders.js
@@ -0,0 +1,27 @@
+const Order = [
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'foldable Mini Drone',
+        productNumber: '8531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'paid',
+        shipping: 'Delivered'
+    },
+]
+
